3a0ef7f88482fd1ad3bfa4f88dcf16396575acb5
Jason Lewin <34786206+DataBot3000@users.noreply.github.com>
2018-11-15 14:13:48 -0500

    Working code commit

--- BlockChain.py
+++ BlockChain.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: ascii -*-
 """
 Blockchain project
 Created on Fri Oct 26 11:49:17 2018
@@ -10,20 +10,20 @@ import datetime as date
 
 #Define Snakecoin block 
 class Block:
-    def __init__(self, index, timestamp, data, previous_hash):
-        self.index=index
-        self.timestamp= timestamp
-        self.data=data
-        self.previous_hash = previous_hash
-        self.hash = self.hash_block()
-        
-    def hash_block(self):
-        sha = hasher.sha256()
-        sha.update(str(self.index) +
-                   str(self.timestamp) +
-                   str(self.data) +
-                   str(self.previous_hash))
-        return sha.hexdigest()
+  def __init__(self, index, timestamp, data, previous_hash):
+    self.index = index
+    self.timestamp = timestamp
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.hash_block()
+  
+  def hash_block(self):
+    sha = hasher.sha256()
+    sha.update((str(self.index) + 
+    str(self.timestamp) + 
+    str(self.data) + 
+    str(self.previous_hash)).encode()) #change here
+    return sha.hexdigest()
 
 # Generate Genesis block (i.e. first block)
 def create_genesis_block():
@@ -52,5 +52,5 @@ for i in range(0, num_of_blocks_to_add):
     blockchain.append(block_to_add)
     previous_block = block_to_add
     # Print statements showing that the block chain is working
-    print("Block #{} has been added to the blockchain!").format(block_to_add.index)
-    print("Hash: {}\n").format(block_to_add.hash)
+    print("Block #{} has been added to the blockchain!".format(block_to_add.index))
+    print("Hash: {}\n".format(block_to_add.hash))
